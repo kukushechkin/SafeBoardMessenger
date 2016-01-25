a347031cd310e991aaef34cb71fafd57fe03a8cd
Evgeny Eliseev <Evgeny.Eliseev@kaspersky.com>
2016-01-25 19:09:27 +0300

    Updated Android makefiles

--- Components/messenger/projects/android/Android.mk
+++ Components/messenger/projects/android/Android.mk
@@ -1,21 +1,37 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := messenger
+LOCAL_MODULE := libmessenger
 
 LOCAL_C_INCLUDES := include \
 					../../External \
 					
 LOCAL_SRC_FILES := ../../source/messenger_impl.cpp \
-					../../source/history_impl.cpp
+					../../source/detail/base64.cpp \
+					../../source/detail/uuid.cpp \
+					../../source/xmpp/xmpp_connection.cpp \
+					../../source/xmpp/xmpp_context.cpp \
+					../../source/xmpp/xmpp_env.cpp \
+					../../source/xmpp/xmpp_logger.cpp \
+					../../source/xmpp/xmpp_stanza.cpp \
+					../../source/xmpp/xmpp_stanza_builder.cpp \
+					../../source/xmpp/xmpp_stanza_id.cpp \
+					../../source/xmpp/xmpp_stanza_parser.cpp
 
 LOCAL_STATIC_LIBRARIES := libstrophe libexpat
-LOCAL_LDLIBS := -llog
 
 ifeq ($(strip $(NDK_DEBUG)),1)
 	LOCAL_CFLAGS += -D_DEBUG
 endif
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
+
+COPY_TO_PREBUILT_$(TARGET_ARCH_ABI): SRC := $(LOCAL_PATH)/../../obj/local/$(TARGET_ARCH_ABI)/*.a
+COPY_TO_PREBUILT_$(TARGET_ARCH_ABI): DEST := $(LOCAL_PATH)/../../lib/android/$(TARGET_ARCH_ABI)/
+COPY_TO_PREBUILT_$(TARGET_ARCH_ABI): $(LOCAL_MODULE)
+	$(info Copy $(SRC) to $(DEST) )
+	$(shell $(call host-cp,$(SRC),$(DEST)))
+  
+all: COPY_TO_PREBUILT_$(TARGET_ARCH_ABI)
 
 include ../../External/libstrophe/jni/Android.mk
--- Components/messenger/projects/android/Application.mk
+++ Components/messenger/projects/android/Application.mk
@@ -1,5 +1,5 @@
 APP_PROJECT_PATH := $(call my-dir)
-APP_STL := c++_shared
+APP_STL := c++_static
 APP_ABI := armeabi armeabi-v7a x86
 APP_PLATFORM := android-15
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
